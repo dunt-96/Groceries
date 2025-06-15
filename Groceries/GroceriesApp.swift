8938b8575bdcc684455a0ca916823ee06cf5fe65
du <ntd160296@gmail.com>
2025-06-15 16:55:48 +0700

    Initial Commit

---
+++ Groceries/GroceriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceriesApp.swift
+//  Groceries
+//
+//  Created by ntd on 6/15/25.
+//
+
+import SwiftUI
+
+@main
+struct GroceriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
